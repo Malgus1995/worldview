cf41fcfc95559252b20732a1cf228ac05c1442ae
Jason Kent <jasontkent@gmail.com>
2019-12-09 14:14:42 -0500

    handle multiple collection ids

--- tasks/python3/processCollections.py
+++ tasks/python3/processCollections.py
@@ -11,17 +11,32 @@ prog = os.path.basename(__file__)
 product_dict = {}
 wv_product_dict = {}
 
-def validate_concept_id(concept_id):
-  if concept_id is not "TBD" and concept_id is not "N/A" and concept_id is not None:
-    return True
+parser = OptionParser(usage="Usage: %s <input_dir> <output_file>" % prog)
+(options, args) = parser.parse_args()
+input_dir = args[0]
+output_file = args[1]
 
+# Validate that the concept_id value is not a placeholder and handle multiple values
+def validate_add_concept_id(prod_id, concept_id):
+  if concept_id.startswith("TBD") or concept_id.startswith("N/A"):
+    return
+  if concept_id is not None:
+    split_ids = concept_id.split()
+    # Some values have spaces, sometimes spaces represent separate IDs
+    # Here we are assuming if it starts with a C, each value is a separate ID
+    if (len(split_ids) > 1) and all(c_id.startswith('C') for c_id in split_ids):
+      product_dict[prod_id].update({ 'concept-id': split_ids })
+    else:
+      product_dict[prod_id].update({ 'concept-id': concept_id })
+
+# Output dict should have WV product ID as keys, concept ids as values
 def format_output_dict():
   for val in product_dict.values():
-    concept_id_val = val.get('concept-id')
-    if validate_concept_id(concept_id_val):
-      wv_id_val = val.get('wv-id')
+    if val.get('concept-id'):
+      wv_id_val = val['wv-id']
       wv_product_dict[wv_id_val] = val['concept-id']
 
+# Get the product ids and concept ids from the respective sheets and add to product_dict
 def get_values_from_sheets(prod_id_sheet, prod_metadata_sheet):
   # TODO should not set arbitrary max number of rows but instead
   # read as many non-empty rows as we can find
@@ -35,16 +50,9 @@ def get_values_from_sheets(prod_id_sheet, prod_metadata_sheet):
     prod_id = md_row[0].value
     concept_id = md_row[1].value
     if prod_id is not None and concept_id is not None:
-      print(concept_id.split())
-      product_dict[prod_id].update({ 'concept-id': concept_id })
+      validate_add_concept_id(prod_id, concept_id)
 
 #MAIN
-parser = OptionParser(usage="Usage: %s <input_dir> <output_file>" % prog)
-
-(options, args) = parser.parse_args()
-input_dir = args[0]
-output_file = args[1]
-
 for root, dirs, files in os.walk(input_dir):
   for file in files:
     fp = os.path.join(input_dir, file)
@@ -59,7 +67,6 @@ for root, dirs, files in os.walk(input_dir):
 
 with open(output_file, "w") as fp:
   format_output_dict()
-  print(output_file)
   json.dump(wv_product_dict, fp)
 
 print("%s: Mapped %s collections to products in %s" % (
